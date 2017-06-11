4c2197d6d67a8e6bda0c528f36c88b2ed69e2593
paddymcglone <paddymcglone@gmail.com>
2017-06-11 13:52:49 +0100

    Updating class library to point to correct verison of Net framework

--- eManager/eManager.Web/Controllers/HomeController.cs
+++ eManager/eManager.Web/Controllers/HomeController.cs
@@ -1,38 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace eManager.Web.Controllers
 {
-    public class MyController : Controller
+    public class HomeController : Controller
     {
-        public string SayHello()
+        public ActionResult Index()
         {
-            return "Hello world && Hi Paddy";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
     }
-
-    //public class HomeController : Controller
-    //{
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
-
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
-    //}
 }
