e706b1d1058c424ae217677abcd67700cd73f85e
paddymcglone <paddymcglone@gmail.com>
2017-06-12 18:00:11 +0100

    Adding DBContext

--- eManager/eManager.Web/Infrastructure/DepartmentDB.cs
+++ eManager/eManager.Web/Infrastructure/DepartmentDB.cs
@@ -1,8 +1,15 @@
-﻿using System.Data.Entity;
+﻿using eManager.Domain;
+using System.Data.Entity;
+using System.Linq;
 
 namespace eManager.Web.Infrastructure
 {
-    public class DepartmentDB : DbContext
+    public class DepartmentDB : DbContext, IDepartmentDataSource
     {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        IQueryable<Employee> IDepartmentDataSource.Employees => Employees;
+        IQueryable<Department> IDepartmentDataSource.Departments => Departments;
     }
 }
