35a176f6eb04b241571f6863fc308d44d19583d4
paddymcglone <paddymcglone@gmail.com>
2017-06-14 20:47:12 +0100

    Adding Create employee as well as overriding the editorFor textboxes

---
+++ eManager/eManager.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using eManager.Web.Models;
+using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Create(int departmentId)
+        {
+            var viewModel = new CreateEmployeeViewModel {DepartmentId = departmentId};
+            return View(viewModel);
+        }
+    }
+}
---
+++ eManager/eManager.Web/Models/CreateEmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace eManager.Web.Models
+{
+    public class CreateEmployeeViewModel
+    {
+        [HiddenInput]
+        public int DepartmentId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required, DataType(DataType.Date)]
+        public DateTime HireDate { get; set; }
+    }
+}
